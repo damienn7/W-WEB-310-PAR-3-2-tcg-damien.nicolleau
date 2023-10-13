53efbd65e2ecfc554cfe3cd1e1a7a7cf92e4c6bb
damienn7 <damien.nicolleau@epitech.eu>
2023-10-13 17:14:27 +0200

    ADD - SECOND PART / class Deck [DEVELOPED && TESTED]

---
+++ new_tcg/spec/deck.spec.js
@@ -0,0 +1,93 @@
+import Deck from '../src/models/deck';
+
+describe("Unit Test - tests de la couche métier", function () {
+    var number = 0;
+    var model;
+    beforeEach(function () {
+        number++;
+        console.log("ModelTesting beginned [" + number + "]");
+        model = new Deck({ "cards": [{ "face": "card-1" }, { "face": "card-2" }, { "face": "card-3" }, { "face": "card-4" }, { "face": "card-5" }, { "face": "card-6" }] });
+    });
+    describe("Desk Test - testing methods", function () {
+        beforeEach(function () {
+            console.log("Testing function ...");
+        });
+        // TEST - shuffle()
+        describe("Shuffle function ...", function () {
+            beforeEach(function () {
+                console.log("Calling shuffle() ...");
+            });
+            it("Cards are shuffled", function () {
+                console.log('Cards before shuffle() :');
+                console.table(model.cards);
+                let result = model.shuffle();
+                expect(result).toBe(true);
+                console.log('Cards after shuffle() :');
+                console.table(model.cards);
+            });
+            afterEach(function () {
+                console.log("Method testing ended!");
+            });
+        });
+        // TEST insertAt()
+        describe("insertAt function ...", function () {
+            beforeEach(function () {
+                console.log("Calling insertAt() ...");
+            });
+            it("Cards are inserted", function () {
+                console.log('Cards before insertAt() :');
+                console.table(model.cards);
+                let pos = model.insertAt({ "face": "card-2.5" }, 2);
+                console.log(pos);
+                expect(JSON.stringify(model.cards[model.cards.length - 1])).not.toBe(JSON.stringify({ "face": "card-2.5" }));
+                console.log('Cards after insertAt() with position :');
+                console.table(model.cards);
+                model.insertAt({ "face": "card-2.5" });
+                expect(JSON.stringify(model.cards[model.cards.length - 1])).toBe(JSON.stringify({ "face": "card-2.5" }));
+                console.log('Cards after insertAt() without position :');
+                console.table(model.cards);
+            });
+            afterEach(function () {
+                console.log("Call ended");
+            });
+        });
+
+        // TEST insertAt()
+        describe("draw function ...", function () {
+            beforeEach(function () {
+                console.log("Calling insertAt() ...");
+            });
+            it("Card drawed", function () {
+                let result = model.draw();
+                expect(result).not.toBe(false);
+                console.log(JSON.stringify(result));
+                console.table(model.cards);
+            });
+            afterEach(function () {
+                console.log("Call ended");
+            });
+        });
+
+    // TEST getCardsCount()
+    describe("getCardsCount function ...", function () {
+        beforeEach(function () {
+            console.log("Calling getCardsCount() ...");
+        });
+        it("Cards count", function () {
+            let result = model.getCardsCount();
+            expect(result).toBe(model.cards.length);
+            console.log('Length : '+result);
+        });
+        afterEach(function () {
+            console.log("Call ended");
+        });
+    });
+    afterEach(function () {
+        console.log("Testing function ended");
+    });
+});
+
+afterEach(function () {
+    console.log("Model Testing ended [" + number + "]");
+});
+});
---
+++ new_tcg/src/models/deck.js
@@ -0,0 +1,57 @@
+
+export default class Deck {
+    constructor (config) {
+        this.cards = config.cards;
+    }
+
+    shuffle () {
+        // si le paquet est vide, on arrête l'action de mélanger les cartes
+        if (this.getCardsCount() === 0) {
+            return false;
+        }
+        // sinon, on créer une copie de l'array des cartes à mélanger 
+        var cards = [...this.cards];
+        // on itère sur le nombre de cartes dans le paquet
+        for (let index = 0; index < cards.length; index++) {
+            // on échange ici deux cartes du paquet à chaque itération 
+            // avec l'aide d'un index random compris entre 0 
+            // et le nombre de cartes 
+            const element = cards[index];
+            const random_index = Math.floor(Math.random() * cards.length);
+            let element_to_shuffle = cards[random_index];
+
+            cards[random_index] = element;
+            cards.splice(index, 1,element_to_shuffle);
+            // cards[index] = element_to_shuffle;
+        }
+        // on initialise nouveau paquet fraichement mélangé
+        this.cards = cards;
+
+        return true;
+    }
+
+    insertAt(card, position = null) {
+        // ajoute une carte à la fin du paquet 
+        // ou à la position donnée en paramètre
+        // !position -> position === null
+        if (!position) {
+            this.cards.push(card);
+        } else {
+            this.cards.splice(position,0,card);
+        }
+    }
+
+    draw () {
+        // retourne la première carte du paquet
+        if (this.cards.length === 0) {
+            return false;
+        } else {
+            return this.cards.splice(0,1);
+        }
+    }
+
+    getCardsCount () {
+        // on retourne le nombre de cartes dans le paquet
+        return this.cards.length;
+    }
+}
